9cd4ba903d673128340228b8ed1e88662c63df2b
Ashish J shetty <ashishshetty2612@gmail.com>
2020-12-13 11:40:36 +0000

    Completed work

--- index.js
+++ index.js
@@ -5,11 +5,57 @@ let score=0;
 
 const questions=[
 {
-  question: "Where do I live? ",
+  question: "Where do I live? :",
   answer: "Mangalore"
 },
 {
-  question: "What is my favorite color? ",
+  question: "What is Ashish's favorite color? : ",
   answer: "black"
+},
+{
+  question:"where do Ashish work ? :",
+  answer:"Maersk"
 }
+
 ]
+
+
+function welcome(){
+  var user=readLineSync.question("What is your name:");
+
+  console.log("Welcome "+user+",Lets start quiz about Ashish");
+}
+
+
+function play(question,answer){
+  var userAnswer=readLineSync.question(question);
+
+  if(answer.toLowerCase()===userAnswer.toLowerCase()){
+    console.log("Right answer !!!");
+    score=score+1;
+  }else{
+    console.log("Wrong answer :(");
+  }
+
+
+  console.log("Current score: "+score);
+
+}
+
+function game(){
+  for(let i=0;i<questions.length;i++){
+    play(questions[i].question,questions[i].answer);
+  }
+}
+
+
+function showScore(){
+ console.log("Yayy, your score is :"+score);
+}
+
+
+
+
+welcome();
+game();
+showScore();
