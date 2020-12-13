d1da144c2e00eb5a0034300ea4f3bef238614a8f
Ashish J shetty <ashishshetty2612@gmail.com>
2020-12-13 11:23:30 +0000

    Feat:Index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+var readLineSync=require('readline-sync')
+
+let score=0;
+
+
+const questions=[
+{
+  question: "Where do I live? ",
+  answer: "Mangalore"
+},
+{
+  question: "What is my favorite color? ",
+  answer: "black"
+}
+]
